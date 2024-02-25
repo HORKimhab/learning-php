f093d2a68a3a704c3b3facc1ba86a9d33c293b76
HORKimhab <hkimhab@mango-byte.com>
2024-02-25 11:04:13 +0700

    feat: inheritance and visibility - 04

--- oop/includes/newclass.inc.php
+++ oop/includes/newclass.inc.php
@@ -1,7 +1,22 @@
 <?php
 class NewClass{
+    
     // Properties and Method goes here
     public $info = "This is public info";
 }
+class Person {
+    protected $first = "H";
+    private $last = "Kimhab";
+    private $age = "32";
+}
+
+# Extend from person can use all in Class Person
+class Pet extends Person {
+    public function owner(){
+        $a = $this->first;
+        return $a;
+    }
+}
+
 $object = new NewClass;
-var_dump($object);
+// var_dump($pet);
--- oop/index.php
+++ oop/index.php
@@ -1,3 +1,8 @@
+<?php 
+// Include other_file.php
+require_once './includes/newclass.inc.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +12,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
 <body>
-    <h1>Config Bootstrap 5</h1>
+    <div class="container-fluid">
+        <h1>Learning OOP PHP</h1>
+    </div>
+
+    <?php
+        $pet01 = new Pet(); 
+        $person = new Person(); 
+        echo $person->first;
+        // var_dump($pet01);
+        echo $pet01->owner();
+
+    ?>
+
+   
+
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
